8c6cf209fca67a05e491d4e35ff5bcc91b3120c8
Carmelo Santana <me@carmelosantana.com>
2021-02-09 11:45:49 -0500

    Revert "Added support for full ADF implemenation"
    This reverts commit 5462b0b4d996261aa2dd86447dc5fa864edf3bd7.

--- examples/example-1.php
+++ examples/example-1.php
@@ -3,13 +3,13 @@ require dirname( __DIR__ ) . '/vendor/autoload.php';
 
 // example 1: Minimal lead from ADF version 1.0.
 $adf = ( new carmelosantana\SendADF\SendADF() )
-    ->add_prospect( '' )
+    ->add_prospect()
     ->add_node( 'requestdate', '2000-03-30T15:30:20-08:00' )
     ->add_vehicle( [
         'year' => 1999,
         'make' => 'Chevrolet',
         'model' => 'Blazer'
-    ], '', '' )
+    ] )
     ->add_customer()
         ->add_contact()
             ->add_name( 'John Doe', 'full' )
