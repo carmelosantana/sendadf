8b590ca2aef6262df700e0c9a3e6d34054da7046
Carmelo Santana <me@carmelosantana.com>
2021-02-09 17:44:00 -0500

    Changed set_requestdate() to add_requestdate()
    Added empty fields per ADF 1.0 example

--- examples/example-1.php
+++ examples/example-1.php
@@ -4,7 +4,7 @@ require dirname( __DIR__ ) . '/vendor/autoload.php';
 // example 1: Minimal lead from ADF version 1.0.
 $adf = ( new carmelosantana\SendADF\SendADF() )
     ->add_prospect( '' )
-    ->add_node( 'requestdate', '2000-03-30T15:30:20-08:00' )
+    ->add_requestdate( '3/30/2000 3:30PM' )
     ->add_vehicle( [
         'year' => 1999,
         'make' => 'Chevrolet',
@@ -12,10 +12,10 @@ $adf = ( new carmelosantana\SendADF\SendADF() )
     ], '', '' )
     ->add_customer()
         ->add_contact()
-            ->add_name( 'John Doe', 'full' )
-            ->add_phone( '393-999-3922' )
+            ->add_name( 'John Doe', '', '' )
+            ->add_phone( '393-999-3922', '', '' )
     ->add_vendor()
         ->add_contact()
-            ->add_name( 'Acura of Bellevue', 'full' );
+            ->add_name( 'Acura of Bellevue', '', '' );
 
 echo $adf->getXML();
