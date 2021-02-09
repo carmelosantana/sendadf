5c664087f3fb13d1ade54a021c7c658f9245d522
Carmelo Santana <me@carmelosantana.com>
2021-02-09 09:04:53 -0500

    Added complete specification from ADF 1.0
    Changed parameters() to element()
    Removed by_version()

--- src/Spec.php
+++ src/Spec.php
@@ -4,85 +4,307 @@ declare(strict_types=1);
 namespace carmelosantana\SendADF;
 
 class Spec {
-    private $email = [
-        '@attributes' => [
-            'preferredcontact'
-        ]
-    ];    
-
-    private $id = [
-        '@attributes' => [
-            'source'
-        ]
+    /** @var array ADF */
+    private $adf = [
+        'prospect'    
     ];
 
-    private $name = [
+    /** @var array Prospect */
+    private $prospect = [
         '@attributes' => [
-            'part'
-        ]
-    ];
-
-    private $phone = [
-        '@attributes' => [
-            'type'
-        ]
-    ];    
-
-    private $contact = [
-        '@attributes' => [
-            'primarycontact'
-        ],
-        'name' => [
-            '@attributes' => [
-                'part'
+            'status' => [
+                'new',
+                'resend'
             ]
-        ],
-        'email',
-        'phone',
-        'address' => [
-            'street' => [
-                '@attributes' => [
-                    'line'
-                ]
-            ],
-            'apartment',
-            'city',
-            'regioncode',
-            'postalcode',
-            'country',
-        ]
+        ],        
+        'id',
+        'requestdate',
+        'vehicle',
+        'customer',
+        'vendor',
+        'provider',
     ];
 
+    /** @var array Vehicle */
     private $vehicle =  [
         '@attributes' => [
-            'interest',
-            'status'
+            'interest' => [
+                'buy',
+                'lease',
+                'sell',
+                'trade-in',
+                'test-drive',
+            ],
+            'status' => [
+                'new',
+                'used'
+            ]
         ],
         'id',
-        'year',     // required
-        'make',     // required
-        'model',    // required
+        'year',
+        'make',
+        'model',
         'vin',
         'stock',
         'trim',
         'doors',
         'bodystyle',
         'transmission',
-        'odometer',
-        'condition' => [
-            'excellent',
-            'good',
-            'fair',
-            'poor',
-            'unknown'
-        ],
-        'colorcombination' => [],
+        'condition',
         'price',
         'pricecomments',
-        'option' => [],
-        'finance' => [],
+        'comments',
+        // attlist + elements
+        'odometer',
+        'colorcombination',
+        'imagetag',
+        'option',
+        'finance',
+    ];
+
+    private $odometer = [
+        '@attributes' => [
+            'status' => [
+                'unknown',
+                'rolledover',
+                'replaced',
+                'original'
+            ],
+            'units' => [
+                'km',
+                'mi'
+            ]
+        ]
+    ];
+    
+    private $colorcombination = [
+        'interiorcolor',
+        'exteriorcolor',
+        'preference'
+    ];
+
+    private $imagetag = [
+        '@attributes' => [
+            'width',
+            'height',
+            'alttext'
+        ]
+    ];
+
+    private $option = [
+        'optionname',
+        'manufacturercode',
+        'stock',
+        'weighting',
+        'price'
+    ];
+
+    /** @var array Finance */
+    private $finance = [
+        'method',
+        'amount',
+        'balance'
+    ];
+
+    private $amount = [
+        '@attributes' => [
+            'type' => [
+                'downpayment',
+                'monthly',
+                'total'
+            ],
+            'limit' => [
+                'maximum',
+                'minimum',
+                'exact'
+            ],
+            'currency'
+        ]
+    ];
+
+    private $balance = [
+        '@attributes' => [
+            'type' => [
+                'finance',
+                'residual'
+            ],
+            'currency'
+        ]
+    ];
+
+    /** @var array Customer */
+    private $customer = [
+        'contact',
+        'id',
+        'timeframe',
         'comments'
     ];
+
+    private $timeframe = [
+        'description',
+        'earliestdate',
+        'latestdate',
+    ];
+
+    /** @var array Vendor */
+    private $vendor = [
+        'id',
+        'vendorname',
+        'url',
+        'contact'
+    ];
+
+    /** @var array Provider */
+    private $provider = [
+        'id',
+        'name',
+        'service',
+        'url',
+        'email',
+        'phone',
+        'contact'
+    ];    
+
+    /** @var array Contact */
+    private $contact = [
+        '@attributes' => [
+            'primarycontact' => [
+                1,
+                0
+            ]
+        ],
+        'name',
+        'email',
+        'phone',
+        'address'
+    ];
+
+    private $name = [
+        '@attributes' => [
+            'part' => [
+                'first',
+                'middle',
+                'suffix',
+                'last',
+                'full'
+            ],
+            'type' => [
+                'individual',
+                'business'
+            ]
+        ]
+    ];
+
+    private $phone = [
+        '@attributes' => [
+            'type' => [
+                'phone',
+                'fax',
+                'cellphone',
+                'pager'
+            ],
+            'time' => [
+                'morning',
+                'afternoon',
+                'evening',
+                'nopreference',
+                'day'
+            ],
+            'preferredcontact' => [
+                0,
+                1
+            ]
+        ]
+    ];    
+
+    private $email = [
+        '@attributes' => [
+            'preferredcontact' => [
+                0,
+                1
+            ]
+        ]
+    ];    
+
+    private $address = [
+        '@attributes' => [
+            'type' => [
+                'work',
+                'home',
+                'delivery'
+            ]
+        ],        
+        'street',
+        'apartment',
+        'city',
+        'regioncode',
+        'postalcode',
+        'country',
+    ];
+
+    private $street = [
+        '@attributes' => [
+            'line' => [
+                1,
+                2,
+                3,
+                4,
+                5
+            ]
+        ]
+    ];
+
+    /** @var array Price */
+    private $price = [
+        '@attributes' => [
+            'type' => [
+                'quote',
+                'offer',
+                'msrp',
+                'invoice',
+                'call',
+                'appraisal',
+                'asking'
+            ],
+            'currency',
+            'delta' => [
+                'absolute',
+                'relative',
+                'percentage'
+            ],
+            'relativeto' => [
+                'msrp',
+                'invoice'
+            ],
+            'source'
+        ]        
+    ];
+
+    /** @var array Shared */
+    private $id = [
+        '@attributes' => [
+            'sequence',
+            'source'
+        ]
+    ];
+
+    private $comments = [];
+
+    private $stock = [];
+
+    private $url = [];
+
+    /**
+     * Checks if XML element is part of specification.
+     *
+     * @param string $node XML element
+     * @return void
+     */
+    public function element( $node ): array
+    {
+        if ( isset($this->$node) )
+            return $this->$node;
+    }
 
     /**
 	 * Initialize instance
@@ -92,39 +314,5 @@ class Spec {
 	public static function get()
 	{
 		return new static();
-    }
-
-    public function parameters( $node )
-    {
-        return $this->$node;
-    }
- 
-    public function by_version( $format='adf', $version='1.0' )
-    {
-        switch ( $format ){
-            default:
-                if ( version_compare( $version, '1.0', '>=' ) ){
-                    return [
-                        'prospect' => [
-                            'id',
-                            'type',
-                            'vehicle' => $this->vehicle,
-                            'customer' => [
-                                'contact' => $this->contact,
-                            ],
-                            'vendor' => [
-                                'id' => $this->id,
-                                'vendorname'
-                            ],
-                            'provider' => [
-                                'id' => $this->id,
-                                'name' => $this->name,
-                                'service'
-                            ]
-                        ]
-                    ];
-                }            
-            break;
-        }
     }
 }
